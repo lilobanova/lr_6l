88cb53911eb0e42ac910ccf4069406a611e6f4b4
lilobanova <90153650+lilobanova@users.noreply.github.com>
2022-01-09 04:47:08 +0300

    Delete ind1.py

--- py6/ind1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-if __name__ == '__main__':
-    s = input("Введите предложение: ")
-    n = s.count('нн')
-    print(f"В предложение нн повторяется {n} раз")
